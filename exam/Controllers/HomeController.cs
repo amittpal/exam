7d986aff80dafd4382d48f9360ca51c345e35224
Amitt <amitrailwayservice@gmail.com>
2018-08-10 14:40:30 +0530

    Add project files.

---
+++ exam/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace exam.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult term()
+        {
+            return View();
+        }
+        public ActionResult setting()
+        {
+            return View();
+        }
+    }
+}
